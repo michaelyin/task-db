a0f5f6f6acdd9372e1b21ed085300cf446e4ab6c
michaelyin <gt8442b@yahoo.com>
2016-11-15 17:15:02 -0500

    real standardtype populated.

--- services/src/main/java/net/wyun/qys/domain/standard/StandardType.java
+++ services/src/main/java/net/wyun/qys/domain/standard/StandardType.java
@@ -3,10 +3,28 @@ package net.wyun.qys.domain.standard;
 import java.util.EnumSet;
 
 public enum StandardType {
-	IMB(0), 
-    Postnet(1),
-    Planet(2),
-    Unknown(3);
+	安全(1), 
+	车身及悬架(2),
+	车轮(3),
+	材料(4),
+	灯光(5),
+	底盘(6),
+	汽车电子与电磁兼容(7),
+	电器(8),
+	防盗(9),
+	回收利用(10),
+	环保不含排放(11),
+	节能(12),
+	排放(13),
+	制动(14),
+	电动车辆(15),
+	挂车(16),
+	客车(17),
+	专用车(18),
+	液化石油气汽车(19),
+	燃气汽车(20),
+	仪表(21),
+	其他(22);
 	
 	int value;
 	
@@ -25,7 +43,7 @@ public enum StandardType {
 			}
 		}
 		
-		return Unknown;
+		return 其他;
 	}
 
 }
