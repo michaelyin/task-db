8f38eed94bb568f8d83fe0c4733d1abd201cc5ee
michaelyin <gt8442b@yahoo.com>
2016-12-02 00:57:44 -0500

    db creation xml files update.

--- services/src/test/java/org/exoplatform/task/TestUtils.java
+++ services/src/test/java/org/exoplatform/task/TestUtils.java
@@ -76,9 +76,11 @@ public class TestUtils {
     Database database = DatabaseFactory.getInstance()
         .findCorrectDatabaseImplementation(new JdbcConnection(conn));
 
-    liquibase = new Liquibase("db/changelog/qys.db.policy.changelog-2.0.0.xml", new ClassLoaderResourceAccessor(), database);
+    liquibase = new Liquibase("db/changelog/qys.db.npolicy.changelog-1.0.0.xml", new ClassLoaderResourceAccessor(), database);
     liquibase.update((String)null);
-    liquibase = new Liquibase("db/changelog/qys.db.stan.changelog-2.0.1.xml", new ClassLoaderResourceAccessor(), database);
+    liquibase = new Liquibase("db/changelog/qys.db.lpolicy.changelog-1.0.1.xml", new ClassLoaderResourceAccessor(), database);
+    liquibase.update((String)null);
+    liquibase = new Liquibase("db/changelog/qys.db.stan.changelog-1.0.2.xml", new ClassLoaderResourceAccessor(), database);
     liquibase.update((String)null);
   }
 
